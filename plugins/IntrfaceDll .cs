132eff7a0e4d66f4d5348fc84c3bd93d5603eb4d
yosi058 <yosefnatanb@gmail.com>
2021-04-14 10:16:17 +0300

    update the dll files in the plugins folder
    update the files to a another folder in FilesUploading

---
+++ plugins/IntrfaceDll .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMinCircleLib
+{
+    public interface IntrfaceDll
+    {
+        UserControl1 create();
+        void update(List<float> p1_train, List<float> p2_train, List<float> p1_test, List<float> p2_test);
+        void time(int index);
+
+
+    }
+}
